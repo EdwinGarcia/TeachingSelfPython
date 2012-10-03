64110f1e6647fefa290c944b3a4060990bdc7fae
Edwin <edwing@gmail.com>
2012-10-02 19:58:02 -0700

    Function Template
    This is the template in which I will be learning how to write Python
    functions in.

---
+++ FunctionTemplate.py
@@ -0,0 +1,18 @@
+#header
+def are(base, height):
+
+#Type Contract
+	'''(number, number) -> number
+
+#Description
+	Return the area of a triangle with dimensions base and height.
+
+#Examples
+	>>> area(10, 5)
+	25.0
+	>>> area(2.5, 3)
+	3.75
+	'''
+
+#body
+return base * height / 2
