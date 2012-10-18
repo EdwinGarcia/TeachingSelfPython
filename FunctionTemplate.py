e6232bb5a52b179488134158f17b863c9e7b2270
Edwin <edwing@gmail.com>
2012-10-18 01:19:48 -0700

    Fixed Typo.
    Fixed type on line 12 (def are to def area).

--- FunctionTemplate.py
+++ FunctionTemplate.py
@@ -9,7 +9,7 @@
 # If you write out your functions in this order, you can think about how things will work logically and it flows naturally.
 
 #header
-def are(base, height):
+def area(base, height):
 
 #Type Contract
 	'''(number, number) -> number
